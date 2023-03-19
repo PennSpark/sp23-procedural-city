ed5bdb075e1d6014fd2e533ad738a15b3e9508eb
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2023-03-19 00:37:24 -0400

    fixed some index out of bounds issues and added more procedural generation for sets (based on face types)

--- tile.py
+++ tile.py
@@ -1,66 +1,85 @@
 class Tile:
+    keys = [0, 1, 2, 3, 4, 5]
+    vals = [1, 0, 3, 2, 5, 4]
+    names = ["up", "down", "west", "east", "north", "south"]
+    dir_map = dict(zip(keys, vals))
+    name_map = dict(zip(keys, names))
     # Define tile name, initialize empty direction sets
-    def __init__(self, name):
+    def __init__(self, name, faces = ["", "", "", "", "", ""]):
         self.name = name
+        self.faces = faces
         self.west_set = set()
         self.east_set = set()
         self.north_set = set()
         self.south_set = set()
         self.up_set = set()
         self.down_set = set()
+        self.sets = [self.up_set, self.down_set, self.west_set, self.east_set, self.north_set, self.south_set]
     
     def getTileName(self):
         return self.name
     
     # Add list of tiles to a direction set
     def get_set(self, dir):
-        match dir:
-            case "west":
-                return self.west_set
-            case "east":
-                return self.east_set
-            case "north":
-                return self.north_set
-            case "south":
-                return self.south_set
-            case "up":
-                return self.up_set
-            case "down":
-                return self.down_set
-            case _:
-                raise Exception("invalid direction")
+        if dir == "west":
+            return self.west_set
+        elif dir == "east":
+            return self.east_set
+        elif dir == "north":
+            return self.north_set
+        elif dir == "south":
+            return self.south_set
+        elif dir == "up":
+            return self.up_set
+        elif dir == "down":
+            return self.down_set
+        else:
+            raise Exception("invalid direction")
             
     def add_to_set(self, dir, list):
-        match dir:
-            case "west":
-                self.west_set.update(list)
-            case "east":
-                self.east_set.update(list)
-            case "north":
-                self.north_set.update(list)
-            case "south":
-                self.south_set.update(list)
-            case "up":
-                self.up_set.update(list)
-            case "down":
-                self.down_set.update(list)
-            case _:
-                raise Exception("invalid direction")
+        if dir == "west":
+            self.west_set.update(list)
+        elif dir == "east":
+            self.east_set.update(list)
+        elif dir == "north":
+            self.north_set.update(list)
+        elif dir == "south":
+            self.south_set.update(list)
+        elif dir == "up":
+            self.up_set.update(list)
+        elif dir == "down":
+            self.down_set.update(list)
+        else:
+            raise Exception("invalid direction")
+    
+    def print_sets(self):
+        print(self.name)
+        for idx, set in enumerate(self.sets):
+            print(f"{Tile.name_map[idx]}: {list(map(Tile.getTileName, list(set)))}")
+
+        
+    
+    def generate_sets(tile1, tile2):
+        faces_1 = tile1.faces
+        faces_2 = tile2.faces
+        for i in range(6):
+            j = Tile.dir_map[i]
+            if faces_1[i] == faces_2[j]:
+                tile1.add_to_set(Tile.name_map[i], [tile2])
             
     def remove_from_set(self, dir, item):
-        match dir:
-            case "west":
-                self.west_set.remove(item)
-            case "east":
-                self.east_set.remove(item)
-            case "north":
-                self.north_set.remove(item)
-            case "south":
-                self.south_set.remove(item)
-            case "up":
-                self.up_set.remove(item)
-            case "down":
-                self.down_set.remove(item)
-            case _:
-                raise Exception("invalid direction")
+        if dir == "west":
+            self.west_set.remove(item)
+        elif dir == "east":
+            self.east_set.remove(item)
+        elif dir == "north":
+            self.north_set.remove(item)
+        elif dir == "south":
+            self.south_set.remove(item)
+        elif dir == "up":
+            self.up_set.remove(item)
+        elif dir == "down":
+            self.down_set.remove(item)
+        else:
+            raise Exception("invalid direction")
             
--- wfc.py
+++ wfc.py
@@ -2,7 +2,7 @@ import heapq
 from tile import Tile
 from block import Block
 import random
-# import maya.cmds as cmds
+#import maya.cmds as cmds
 
 # Tile refers to general tile types
 # Block refers to an individual block in the x by y by z board. Each block may be untiled or tiled.
@@ -20,9 +20,10 @@ class Board:
     def __init__(self, X, Y, Z, tiles):
         self.board = [[[Block(tiles.copy()) for i in range(X) ] for i in range(Y)] for i in range(Z)]
         self.block_heap = []
-        for i in range(X): 
+        self.render_stack = []
+        for i in range(Z): 
             for j in range(Y):
-                for k in range(Z):
+                for k in range(X):
                     heapq.heappush(self.block_heap, (len(tiles), (i, j, k)))
         # print(self.block_heap)
     
@@ -33,9 +34,20 @@ class Board:
                 name_row = map(Tile.getTileName, tile_row)
                 print(*name_row)
 
-    # def place_tile(self, tile, x, y, z):
-    #     cmds.duplicate(f"{tile._name}")
-    #     cmds.move(x+1, y+1, z+1)
+    def push_tile(self, tile, x, y, z):
+        self.render_stack.append((tile, x, y, z))
+
+    def place_tile(self, tile, x, y, z):
+        pass
+        #cmds.duplicate(f"{tile.name}", n=f"{tile.name}_{x}{y}{z}")
+        #cmds.select(f"{tile.name}_{x}{y}{z}")
+        #cmds.move(x, y, z)
+        #cmds.select("Tile1")
+    
+    def render_tiles(self):
+        while self.render_stack:
+            obj = self.render_stack.pop()
+            self.place_tile(obj[0], obj[1], obj[2], obj[3])
 
     def choice(self, x, y, z):
         print("{}, {}, {}".format(x, y, z))
@@ -46,14 +58,14 @@ class Board:
         
         # Assumes possible set of tiles for block is valid
         # Pick a tile from the set
+        print(list(map(Tile.getTileName, block.get_possible_tiles())))
         if len(block.get_possible_tiles()) == 0:
-            # print(block.get_possible_tiles())
-            return
+            return 1
         
         # For now just pick tile at index 0, TODO: add probability distribution for pick
         tile_placed = random.choice(list(block.get_possible_tiles()))
         block.set_tile(tile_placed)
-        # self.place_tile(tile_placed, x, y, z)
+        self.push_tile(tile_placed, x, y, z)
         print("Placed tile " + tile_placed.getTileName() + " at block " + str((x, y, z)))
 
         # Update neighbors' possible tiles
@@ -80,6 +92,8 @@ class Board:
         # z + 1 (Up)
         if self.is_block_inbounds(x, y, z + 1) and not self.board[z + 1][y][x].is_tiled():
             self.board[z + 1][y][x].set_possible_tiles((self.board[z + 1][y][x].get_possible_tiles()).intersection(block.get_tile().get_set("up")))
+        
+        return 0
 
     def is_block_inbounds(self, x, y, z):
         return x < len(self.board[0][0]) and y < len(self.board[0]) and z < len(self.board)
@@ -87,19 +101,32 @@ class Board:
 
 def main():
     # TODO: Board is a 3d array of Block objects. Block objects are initialized with tile type None and with possible tile set of all tiles
-    tile_1 = Tile("Tile1")
-    tile_2 = Tile("Tile2")
-    dirs = ["north", "south", "east", "west", "up", "down"]
-    for dir in dirs:
-        tile_1.add_to_set(dir, [tile_1, tile_2])
-        tile_2.add_to_set(dir, [tile_1, tile_2])
-    board = Board(3, 3, 3, set([tile_1, tile_2]))
+    tile_1 = Tile("Tile1", ['Circle','Circle','Circle','Circle','Circle','Circle'])
+    tile_2 = Tile("Tile2", ['Square','Square','Square','Square','Square','Square'])
+    tile_3 = Tile("Tile3", ['Octogon','Octogon','Octogon','Octogon','Octogon','Octogon'])
+    tile_4 = Tile("Tile4", ['Circle','Octogon','Circle','Octogon','Square','Square'])
+    tile_5 = Tile("Tile5", ['Octogon','Square','Circle','Square','Octogon','Circle'])
+    tile_6 = Tile("Tile6", ['Octogon','Square','Octogon','Circle','Octogon','Circle'])
+    tile_7 = Tile("Tile7", ['Circle','Octogon','Octogon','Circle','Square','Circle'])
+    tile_8 = Tile("Tile8", ['Square','Octogon','Circle','Octogon','Octogon','Circle'])
+    tile_9 = Tile("Tile9", ['Square','Circle','Square','Square','Circle','Octogon'])
+    tile_10 = Tile("Tile10", ['Circle','Circle','Square','Square','Circle','Square'])
+    tiles = [tile_1, tile_2, tile_3, tile_4, tile_5, tile_6, tile_7, tile_8, tile_9, tile_10]
+    for i in range (10):
+        for j in range(10):
+            Tile.generate_sets(tiles[i], tiles[j])
+    #for tile in tiles:
+    #    tile.print_sets()
+    board = Board(1, 1, 3, set(tiles))
 
     while len(board.block_heap) != 0:
         next_block = heapq.heappop(board.block_heap)
-        board.choice(next_block[1][2], next_block[1][1], next_block[1][0])
+        print(next_block[1])
+        if board.choice(next_block[1][2], next_block[1][1], next_block[1][0]):
+            board = Board(1, 1, 3, set(tiles))
     
     board.print_board()
+    board.render_tiles()
     
     
 
